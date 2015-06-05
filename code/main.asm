b7e0d0cb8d317377035f0d9e58fcb96edf7a84c1
Henry J Schmale <hschmale16@gmail.com>
2015-06-05 14:09:04 -0400

    Wrote a real wait function
    * Wrote a wait sub that waits for timer0 to overflow R0
    * Rerouted the Pcb

--- code/main.asm
+++ code/main.asm
@@ -3,10 +3,10 @@
 ; Date: June 1, 2015
 
 LMOTOR1	EQU	P3.5
-LMOTOR2 EQU	P3.7		; Makes Left Motor go in reverse
-RMOTOR1 EQU	P1.2
-RMOTOR2 EQU	P1.3		; Makes Right Motor go in reverse
-BLADEON EQU	P1.4
+LMOTOR2	EQU	P3.7		; Makes Left Motor go in reverse
+RMOTOR1	EQU	P1.2
+RMOTOR2	EQU	P1.3		; Makes Right Motor go in reverse
+BLADEON	EQU	P1.4
 
 main:	ORG	0000H		; Program Entry Point
 	JMP	setup		; Interupt Overwriting Protection
@@ -15,33 +15,25 @@ setup:	ORG	002CH		; executes exactly once
 	SETB	EA
 	JMP	evntlp		; Go straight to the event loop
 ; Define Subroutines below
-swait:				; A very short wait ~5secs
-	CLR	20h
-	CLR	TR0		; Shutoff the timer
-	MOV	TL0,#00h	; Reset Timer0
-	MOV	TH0,#00h
-	SETB	ET0
-	SETB	TR0
-l1wait:	JB	00h,l2wait	; loop in swait
-	JMP	l1wait
-l2wait:	CLR	ET0
+
+; Wait for timer0 to overflow R0 times
+; 
+wait:	SETB	TR0
+	CLR	TF0
+	JNB	TF0, $		; Busy Loop until timer0 overflows
+	DJNZ	R0,wait
 	CLR	TR0
-	RET
-wait:				; Waits at least 5 seconds
-	PUSH	ACC
-	MOV	ACC,#255
-lwait:	CALL	swait
-	DJNZ	ACC,lwait
-	POP	ACC
-	RET
+	CLR	TF0
+myret:	RET			; Just a random return useful to ret by jmpn to
+
 revrnd:				; Reverse and turn around
 	CLR	LMOTOR1
 	CLR	RMOTOR1
 	SETB	LMOTOR2
 	SETB	RMOTOR2
-	CALL	wait
+	MOV	R0, #0FH
+	LCALL	wait
 	CLR	LMOTOR2
-	CALL	swait
 	CLR	RMOTOR2
 	SETB	LMOTOR1
 	SETB	RMOTOR1
